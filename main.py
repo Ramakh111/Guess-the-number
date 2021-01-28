5a85d7f68f979453bd7d513feebfe7926948694f
Rama <78087866+Ramakh111@users.noreply.github.com>
2021-01-28 22:35:49 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from generating_a_random_number import Generating_a_random_number
+from input_from_user import input_from_user
+from checking_the_number import checking_the_number
+
+# Running
+random_number = Generating_a_random_number()
+input_from_user = input_from_user()
+checking_the_number(random_number, input_from_user)
